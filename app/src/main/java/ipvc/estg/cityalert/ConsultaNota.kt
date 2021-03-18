4b12446dc720864ca83bd506df29e9d382aa4680
mrma2498 <mrodriguesaraujo@ipvc.pt>
2021-03-18 19:06:40 +0000

    Início da implementação da funcionalidade "Editar Nota"

--- app/src/main/java/ipvc/estg/cityalert/ConsultaNota.kt
+++ app/src/main/java/ipvc/estg/cityalert/ConsultaNota.kt
@@ -1,11 +1,22 @@
 package ipvc.estg.cityalert
 
 import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import ipvc.estg.cityalert.entities.Nota
 import kotlinx.android.synthetic.main.activity_consulta_nota.*
 
+
 class ConsultaNota : AppCompatActivity() {
+
+    private val editNoteActivityRequestCode = 2
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_consulta_nota)
@@ -15,5 +26,16 @@ class ConsultaNota : AppCompatActivity() {
         val desc: String? = intent.getStringExtra("NoteDes")
         titleNote.text = titulo
         descNote.text = desc
+
+
+        //Abre uma nova atividade para editar a nota
+        val editar = findViewById<FloatingActionButton>(R.id.editNote)
+        editar.setOnClickListener {
+            val intent = Intent(this, EditaNota::class.java)
+            startActivityForResult(intent,editNoteActivityRequestCode)
+
+        }
     }
+
+
 }
---
+++ app/src/main/java/ipvc/estg/cityalert/EditaNota.kt
@@ -0,0 +1,14 @@
+package ipvc.estg.cityalert
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditaNota : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edita_nota)
+    }
+
+
+    /*Código identico ao adicionar nota? mas é necessário fazer a query para fazer update*/
+}
